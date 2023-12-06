045de2c47888f9f690cb0bd04600df3c611e0868
USK0318 <147698680+USK0318@users.noreply.github.com>
2023-12-06 12:13:42 +0000

    commit

--- dummy/src/index.js
+++ dummy/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import Table from './jsx/Table';
+import Dash from './jsx/Dash';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Table/>
+    <Dash/>
   </React.StrictMode>
 );
 
